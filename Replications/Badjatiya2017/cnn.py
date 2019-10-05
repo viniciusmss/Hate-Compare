90b86044930a09f5e51421feb22d979f6d563ad3
Vini Miranda <vini@minerva.kgi.edu>
2019-10-05 15:18:42 +0100

    feat: Print diagnostics during training

--- Replications/Badjatiya2017/cnn.py
+++ Replications/Badjatiya2017/cnn.py
@@ -274,7 +274,9 @@ def train_CNN(X, y, inp_dim, model, weights, epochs=EPOCHS, batch_size=BATCH_SIZ
         y_train = y_train.reshape((len(y_train), 1))
         X_temp = np.hstack((X_train, y_train))
         for epoch in xrange(epochs):
-            for X_batch in batch_gen(X_temp, batch_size):
+            train_loss = 0
+            train_acc = 0
+            for i, X_batch in enumerate(batch_gen(X_temp, batch_size), 1):
                 x = X_batch[:, :sentence_len]
                 y_temp = X_batch[:, sentence_len] # Last column  will be y due to np.hstack
 
@@ -290,9 +292,13 @@ def train_CNN(X, y, inp_dim, model, weights, epochs=EPOCHS, batch_size=BATCH_SIZ
                 except Exception as e:
                     print e
                     print y_temp
-                print x.shape, y.shape
-                loss, acc = model.train_on_batch(x, y_temp, class_weight=class_weights)
-                print loss, acc
+
+                _loss, _acc = model.train_on_batch(x, y_temp, class_weight=class_weights)
+                train_loss += _loss
+                train_acc += _acc
+                if i % 10 == 0:
+                    print "Epoch: %d/%d.\t Batch: %d. Loss: %d. Accuracy: %d" % (epoch,epochs, i, train_loss / i, train_acc/i)
+
         y_pred = model.predict_on_batch(X_test)
         y_pred = np.argmax(y_pred, axis=1)
         print classification_report(y_test, y_pred)
@@ -374,6 +380,6 @@ if __name__ == "__main__":
     data, y = sklearn.utils.shuffle(data, y)
     W = get_embedding_weights()
     model = cnn_model(data.shape[1], EMBEDDING_DIM)
-    train_CNN(data, y, EMBEDDING_DIM, model, W)
+    train_CNN(data, y, EMBEDDING_DIM, model, W, EPOCHS)
 
     pdb.set_trace()
