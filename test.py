fad9337a7697437cf88ee6d74c32afb7059ad3c1
Vinicius Miranda <vini@minerva.kgi.edu>
2019-10-26 10:35:36 +0100

    feat: Test create lookup tables script

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import numpy as np
+import pandas as pd
+
 from preprocess import preprocess
+from create_lookup_tables import create_lookup_tables
+
 
 def _test_preprocess():
 
@@ -9,8 +14,23 @@ def _test_preprocess():
     assert " & MENTIONHERE URLHERE HASHTAGHERE " == \
         preprocess("&amp;@vinimiranda    https://seminar.minerva.kgi.edu     #minerva    ")
 
+def _test_lookup_tables():
+
+    text = pd.Series(["this is a toy", "I mean not really a toy", "I mean a toy vocabulary"])
+    vocab_to_int, int_to_vocab = create_lookup_tables(text)
+
+    # Make sure the dicts make the same lookup
+    missmatches = [(word, id, id, int_to_vocab[id]) for word, id in vocab_to_int.items() if int_to_vocab[id] != word]
+
+    assert not missmatches,\
+        'Found {} missmatche(s). First missmatch: vocab_to_int[{}] = {} and int_to_vocab[{}] = {}'.format(len(missmatches),
+                                                                                                          *missmatches[0])
+
 if __name__ == "__main__":
     print("Testing preprocessing function...")
     _test_preprocess()
 
-    print("All tests were successful.")
+    print("Testing create lookup table function...")
+    _test_lookup_tables()
+
+    print("All tests were successful.\n")
