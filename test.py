b0ffc108751c208c85e4a159218828d1587100d8
Vinicius Miranda <vini@minerva.kgi.edu>
2019-10-26 11:02:07 +0100

    Create padding script and test it.
    Note: Added refactored replication notebook that imports from the
    individual scripts.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from preprocess import preprocess
 from create_lookup_tables import create_lookup_tables
+from padding import create_pad_fn, pad_tweets
 
 
 def _test_preprocess():
@@ -26,11 +27,40 @@ def _test_lookup_tables():
         'Found {} missmatche(s). First missmatch: vocab_to_int[{}] = {} and int_to_vocab[{}] = {}'.format(len(missmatches),
                                                                                                           *missmatches[0])
 
+def _test_pad_tweets():
+
+    assert pad_tweets('hi', 0) == 'hi'
+    assert pad_tweets('hi', 1) == 'hi'
+    assert pad_tweets('hi', 2) == '<PAD> hi'
+    assert len(pad_tweets('hi', 10).split()) == 10
+    assert len(pad_tweets('hi', 100).split()) == 100
+    assert pad_tweets('this sentence is a bit longer', 1) == 'this sentence is a bit longer'
+
 if __name__ == "__main__":
-    print("Testing preprocessing function...")
+
+    df = pd.read_csv("./data/labeled_data.csv", index_col=0)
+    raw_tweets = df.tweet
+    raw_labels = df["class"].values
+
+    print("Testing preprocessing function...\n")
     _test_preprocess()
 
-    print("Testing create lookup table function...")
+    tweets = raw_tweets.map(preprocess)
+    df["clean_tweet"] =  tweets # Get cleaned tweets
+    df["word_count"] = df.clean_tweet.apply(lambda x : len(x.split()))  # Get their word count
+    # Remove outliers
+    old_tweet = df.loc[df.word_count == df.word_count.max(),].tweet.values[0]
+    new_tweet = old_tweet[:old_tweet.find("\r")]
+    df.loc[df.word_count == df.word_count.max(), "tweet"] = new_tweet
+    df.loc[df.word_count == df.word_count.max(), "clean_tweet"] = preprocess(new_tweet)
+    df.loc[df.word_count == df.word_count.max(), "word_count"] = len(preprocess(new_tweet).split())
+
+    print("Testing create lookup table function...\n")
     _test_lookup_tables()
+
+    vocab_to_int, int_to_vocab = create_lookup_tables(tweets)
+
+    print("Testing padding function...\n")
+    _test_pad_tweets()
 
     print("All tests were successful.\n")
